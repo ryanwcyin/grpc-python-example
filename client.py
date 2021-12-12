86b9f601b96799ed4d40ce5537d09feb0b1aad22
Ryan <ryanwcyin@gmail.com>
2021-12-12 12:51:46 +0800

    added server and update port in client

--- client.py
+++ client.py
@@ -4,12 +4,13 @@ from recommendations.recommendations_pb2 import BookCategory, RecommendationRequ
 from recommendations.recommendations_pb2_grpc import RecommendationsStub
 
 def app():
-    channel = grpc.insecure_channel("localhost:50051")
+    channel = grpc.insecure_channel("localhost:8080")
     client = RecommendationsStub(channel)
     request = RecommendationRequest(
         user_id=1, category=BookCategory.SCIENCE_FICTION, max_results=3
     )
-    client.Recommend(request)
+    res = client.Recommend(request)
+    print(res)
 
 if __name__ == '__main__':
     app()
